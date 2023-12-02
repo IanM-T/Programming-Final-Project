a1c7b4476335e9b5301e8b89f650684eaee0d80f
Ian Moore-Timpa <thomas.2017mt@gmail.com>
2023-12-02 16:38:57 -0600

    Working Minesweeper

--- src/src/project.py
+++ src/src/project.py
@@ -1,26 +1,9 @@
 import random
 
-
 board = [[0 for _ in range(10)] for _ in range(10)]
 uncovered_board = [[-1 for _ in range(10)] for _ in range(10)]
-total_mines = int(input("How many Mines?"))
-if total_mines > 25:
-    print("Impossible. Setting to 5 as default.")
-    total_mines = 5
-
-placed_mines = 0
-while placed_mines < total_mines:
-    row = random.randint(0, 9)
-    col = random.randint(0, 9)
-    if board[row][col] == 0:
-        board[row][col] = 1
-        placed_mines += 1
-
 
 def count_adjacent_mines(row, col):
-    """
-    Counts the number of mines surrounding a cell.
-    """
     count = 0
     for i in range(row - 1, row + 2):
         for j in range(col - 1, col + 2):
@@ -30,9 +13,6 @@ def count_adjacent_mines(row, col):
 
 
 def reveal_cell(row, col):
-    """
-    Reveals a cell and its surrounding cells if it's empty.
-    """
     uncovered_board[row][col] = count_adjacent_mines(row, col)
     if uncovered_board[row][col] == 0:
         for i in range(row - 1, row + 2):
@@ -42,15 +22,11 @@ def reveal_cell(row, col):
 
 
 def display_board():
-    """
-    Prints the current state of the board.
-    """
     print("-" * 40)
     for row in range(10):
-        print("|", end="")
         for col in range(10):
             if uncovered_board[row][col] == -1:
-                print(" ", end=" | ")
+                print(end="| ")
             else:
                 print(uncovered_board[row][col], end=" ")
         print("")
@@ -58,33 +34,52 @@ def display_board():
 
 
 def check_win():
-    """
-    Checks if the player has won the game.
-    """
     for row in range(10):
         for col in range(10):
             if board[row][col] == 0 and uncovered_board[row][col] == -1:
                 return False
     return True
 
+def main():
+    total_mines = int(input("How many Mines? (Max 25) "))
+    if total_mines > 25:
+        print("Impossible. Setting to 5 as default.")
+        total_mines = 5
 
-guesses_left = 25 - total_mines
+    placed_mines = 0
 
-display_board()
-while guesses_left > 0 and not check_win():
-    row = int(input("Guess a row (1-10):")) - 1
-    col = int(input("Guess a col (1-10):")) - 1
-    if board[row][col] == 1:
-        print("You hit a Mine.")
-        print("GAME OVER")
+    while placed_mines < total_mines:
+        row = random.randint(0, 9)
+        col = random.randint(0, 9)
+        if board[row][col] == 0:
+            board[row][col] = 1
+            placed_mines += 1
+
+    guesses_left = 25 - total_mines
+    game_over = False
+
+    while guesses_left > 0 and not game_over:
         display_board()
-        break
-    else:
-        reveal_cell(row, col)
-        display_board()
-        guesses_left -= 1
 
-if check_win():
-    print("Congratulations! You won the game.")
-else:
-    print("You ran out of guesses. Better luck next time!")
+        row = int(input("Guess a row (1-10): ")) - 1
+        col = int(input("Guess a col (1-10): ")) - 1
+
+        if board[row][col] == 1:
+            print("You hit a Mine!")
+            print("GAME OVER")
+            game_over = True
+        else:
+            reveal_cell(row, col)
+            guesses_left -= 1
+
+        if check_win():
+            print("Congratulations! You won the game.")
+            game_over = True
+
+    if not game_over:
+        print(f"Better luck next time! ({total_mines} mines)")
+
+    display_board()
+
+if __name__ == "__main__":
+    main()
